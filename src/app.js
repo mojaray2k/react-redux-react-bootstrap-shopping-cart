f250f8020a8c2ea0edcca252ad1a38e3c97300cf
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-13 11:21:30 -0400

    implimented update and delete reducers and actions

--- src/app.js
+++ src/app.js
@@ -15,6 +15,41 @@ const reducer = function (state = { books: [] }, action) {
       // let books = state.books.concat(payload);
       // return { books };
       return { books: [...state.books, ...payload] };
+    case "DELETE_BOOK":
+      // Create a Copy of the current array of books
+      const currentBookToDelete = [...state.books];
+      // determine at which index in the books array is the book to be deleted
+      const indexToDelete = currentBookToDelete.findIndex(function (book) {
+        return book.id === payload.id;
+      });
+      return {
+        books: [
+          ...currentBookToDelete.slice(0, indexToDelete),
+          ...currentBookToDelete.slice(indexToDelete + 1),
+        ],
+      };
+    case "UPDATE_BOOK":
+      // Create a Copy of the current array of books
+      const currentBookToUpdate = [...state.books];
+      // determine at which index in the books array is the book to be update
+      const indexToUpdate = currentBookToUpdate.findIndex(function (book) {
+        return book.id === payload.id;
+      });
+      // Create a new book object with new values and with the same array index of items we want to replace. To achieve this we will use ...spread but we could use concat method too
+      const newBookToUpdate = {
+        ...currentBookToUpdate[indexToUpdate],
+        title: payload.title,
+      };
+      // This log has the purpose to show you how newBookToUpdate looks like
+      console.log("What is it newBookToUpdate", newBookToUpdate);
+      // use slice to remove the book at the specified index, replace with the new object and concatenate with the rest of items in the Array.
+      return {
+        books: [
+          ...currentBookToUpdate.slice(0, indexToUpdate),
+          newBookToUpdate,
+          ...currentBookToUpdate.slice(indexToUpdate + 1),
+        ],
+      };
     default:
       return state;
   }
@@ -54,12 +89,6 @@ store.dispatch({
       description: "This is the second book description",
       price: 50.33,
     },
-  ],
-});
-
-store.dispatch({
-  type: "POST_BOOK",
-  payload: [
     {
       id: 3,
       title: "This is the third book title",
@@ -70,3 +99,16 @@ store.dispatch({
 });
 
 // DELETE and Dispatch Action
+store.dispatch({
+  type: "DELETE_BOOK",
+  payload: { id: 3 },
+});
+
+// UPDATE and Dispatch Action
+store.dispatch({
+  type: "UPDATE_BOOK",
+  payload: {
+    id: 2,
+    title: "Learn React in 24hr",
+  },
+});
